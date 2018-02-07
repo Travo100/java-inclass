4e20287265e12675a710c57cde8a3b30c1530d7c
Travo100 <travo100@gmail.com>
2018-02-06 21:48:01 -0800

    Added classes content

---
+++ src/classes/Classes.java
@@ -0,0 +1,28 @@
+package classes;
+
+public class Classes {
+    public static void main(String[] args) {
+        // Since our User class is in the same package, we don't need to import it.
+        User jim = new User("Jim", "jim@wonderland.edu");
+
+        User olivia = new User("Olivia", "olivia@aol.com");
+
+        // We can call our public methods...
+        jim.introduce();
+
+        olivia.introduce();
+
+        // ...But not private methods.
+        // jim.setEmail("Whoops"); // Throws if you uncomment.
+
+        // Here, we're calling a method on the class per se.
+        System.out.println(User.getCount());
+
+        User throwaway = new User("", "");
+
+        System.out.println(User.getCount());
+
+        // Note that this only exists on the class-NOT on the user objects.
+        // throwaway.getCount(); // Throws if you uncomment.
+    }
+}
